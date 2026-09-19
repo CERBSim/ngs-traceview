e1bc35893ad463dd123df5bbb5fcf2bb8fe165ab
Christopher Lackner <clackner@cerbsim.com>
2026-09-19 08:24:51 +0200

    fix dependencies for deployment on github

--- src/ngs_traceview/appconfig.py
+++ src/ngs_traceview/appconfig.py
@@ -10,7 +10,7 @@ config = AppConfig(
     name="Trace Viewer",
     version = __version__,
     python_class=TraceViewer,
-    frontend_pip_dependencies=[],
+    frontend_pip_dependencies=["numpy", "ngsolve", "ngsolve_webgpu", "ngsolve_gui"],
     description=_DESCRIPTION,
 )
     
